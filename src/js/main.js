cab3fe4e42ca8768e66464744375b90175c2fb66
Pasha Alizarevich <Pasha.sokol1992@icloud.com>
2025-12-10 16:52:11 +0300

    new section nav and welcome

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,20 @@
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 import "../sass/style.scss";
+
+const swiper = new Swiper('.swiper', {
+    slidesPerView: 1,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    modules: [Navigation, Pagination],
+});
