6a1d445f2ab3c666d02c445c02479bf84131c5ac
Pasha Alizarevich <Pasha.sokol1992@icloud.com>
2025-12-11 16:09:38 +0300

    new section virtual and picture

--- src/js/main.js
+++ src/js/main.js
@@ -16,5 +16,52 @@ const swiper = new Swiper('.swiper', {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
       },
+      on: {
+        init (swiper) {
+          updateWelcomeCounter(swiper);
+        },
+        slideChange(wisper) {
+          updateWelcomeCounter(swiper);
+        },
+      },
     modules: [Navigation, Pagination],
 });
+
+// функция обновления счётчика
+function updateWelcomeCounter(swiper) {
+ const counter = document.querySelector('.welcome__counter');
+ if (!counter) return;
+ // текущий слайд (нумерация с 1)
+ const current = String(swiper.realIndex + 1).padStart(2, '0');
+ // общее кол-во слайдов
+ const total = String(swiper.slides.length).padStart(2, '0');
+ // если включишь loop: total можно задать руками, например 5
+ counter.textContent = `${current} | ${total}`;
+}
+
+
+
+import BeerSlider from 'beerslider';
+
+import 'beerslider/dist/BeerSlider.css';
+
+// если Vite, то скрипт уже подключён как type="module" в index.html
+
+document.addEventListener('DOMContentLoaded', () => {
+
+  const sliders = document.querySelectorAll('.beer-slider');
+
+  sliders.forEach(slider => {
+
+    new BeerSlider(slider, {
+
+      // опционально: стартовая позиция в %
+
+      // start: 50,
+
+    });
+
+  });
+
+});
+ 
